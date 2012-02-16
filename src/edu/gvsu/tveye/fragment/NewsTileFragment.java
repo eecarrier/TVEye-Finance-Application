75d702a1be85300d01fd39aa430193670c7e9902
Greg Zavitz <gzavitz@gmail.com>
2012-02-16 14:26:53 -0500

    Relative timestamps for each of the articles now shown on tile

--- src/edu/gvsu/tveye/fragment/NewsTileFragment.java
+++ src/edu/gvsu/tveye/fragment/NewsTileFragment.java
@@ -1,15 +1,20 @@
 package edu.gvsu.tveye.fragment;
 
+import java.util.Date;
+
+import org.apache.http.impl.cookie.DateParseException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.text.format.DateUtils;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,8 +30,7 @@ import edu.gvsu.tveye.util.ImageDownloadTask;
 import edu.gvsu.tveye.util.ImageDownloadTask.ImageCallback;
 
 /**
- * NewsTileFragment is a set of stories seen on the NewsGridActivity
- * screen.
+ * NewsTileFragment is a set of stories seen on the NewsGridActivity screen.
  * 
  * @author gregzavitz
  */
@@ -58,60 +62,90 @@ public class NewsTileFragment extends Fragment {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void setTileOrientation(View view, int orientation) {
-    	this.orientation = orientation;
-    	LinearLayout tiles = (LinearLayout) view.findViewById(R.id.news_tiles);
+		this.orientation = orientation;
+		LinearLayout tiles = (LinearLayout) view.findViewById(R.id.news_tiles);
 		LinearLayout[] tile_group = new LinearLayout[] {
-			(LinearLayout) view.findViewById(R.id.news_tile_row_1),
-			(LinearLayout) view.findViewById(R.id.news_tile_row_2)
-		};
-		tiles.setOrientation(orientation == HORIZONTAL ? LinearLayout.HORIZONTAL : LinearLayout.VERTICAL);
-		tile_group[0].setOrientation(orientation == HORIZONTAL ? LinearLayout.VERTICAL : LinearLayout.HORIZONTAL);
-		tile_group[1].setOrientation(orientation == HORIZONTAL ? LinearLayout.VERTICAL : LinearLayout.HORIZONTAL);
-    }
+				(LinearLayout) view.findViewById(R.id.news_tile_row_1),
+				(LinearLayout) view.findViewById(R.id.news_tile_row_2) };
+		tiles.setOrientation(orientation == HORIZONTAL ? LinearLayout.HORIZONTAL
+				: LinearLayout.VERTICAL);
+		tile_group[0]
+				.setOrientation(orientation == HORIZONTAL ? LinearLayout.VERTICAL
+						: LinearLayout.HORIZONTAL);
+		tile_group[1]
+				.setOrientation(orientation == HORIZONTAL ? LinearLayout.VERTICAL
+						: LinearLayout.HORIZONTAL);
+	}
 
 	public void populate() throws JSONException {
 		LinearLayout[] tile_group = new LinearLayout[] {
-			(LinearLayout) getView().findViewById(R.id.news_tile_row_1),
-			(LinearLayout) getView().findViewById(R.id.news_tile_row_2)
-		};
-		
+				(LinearLayout) getView().findViewById(R.id.news_tile_row_1),
+				(LinearLayout) getView().findViewById(R.id.news_tile_row_2) };
+
 		LayoutInflater inflater = getActivity().getLayoutInflater();
-		for(int i = 0; i < set.length(); i++) {
+		for (int i = 0; i < set.length(); i++) {
 			final JSONObject story = set.getJSONObject(i);
 			float weightSum = tile_group[i / 3].getWeightSum();
-			float tileWeight = i < 3 ? (i == 0 ? weightSum / 2 : weightSum / 4) : weightSum / 3;
+			float tileWeight = i < 3 ? (i == 0 ? weightSum / 2 : weightSum / 4)
+					: weightSum / 3;
 			final View tile = inflater.inflate(R.layout.news_tile, null);
-			tile.setLayoutParams(new LinearLayout.LayoutParams(0, LayoutParams.FILL_PARENT, tileWeight));
+			tile.setLayoutParams(new LinearLayout.LayoutParams(0,
+					LayoutParams.FILL_PARENT, tileWeight));
 			tile.setOnClickListener(new OnClickListener() {
 				public void onClick(View v) {
-					Intent intent = new Intent(getActivity(), NewsArticleActivity.class);
+					Intent intent = new Intent(getActivity(),
+							NewsArticleActivity.class);
 					intent.putExtra("metadata", story.toString());
 					getActivity().startActivity(intent);
 				}
 			});
 
-			final TextView title = (TextView) tile.findViewById(R.id.news_title), 
-			content = (TextView) tile.findViewById(R.id.news_content);
+			final TextView title = (TextView) tile
+					.findViewById(R.id.news_title), content = (TextView) tile
+					.findViewById(R.id.news_content), timestamp = (TextView) tile
+					.findViewById(R.id.news_timestamp);
 			title.setText(Html.fromHtml(story.getString("title")));
 			content.setText(Html.fromHtml(story.getString("content")));
-			if(story.has("imageUrl")) {
-				final ImageView picture = (ImageView) tile.findViewById(R.id.news_picture);
+			timestamp.setText(formatDate(story));
+			if (story.has("imageUrl")) {
+				final ImageView picture = (ImageView) tile
+						.findViewById(R.id.news_picture);
 				new ImageDownloadTask(new ImageCallback() {
 					public void imageFailed(String url) {
 						picture.setVisibility(View.GONE);
 					}
-					
+
 					public void imageDownloaded(String url, Bitmap bitmap) {
-						picture.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.FILL_PARENT, (int)(tile.getMeasuredHeight() * 0.33f)));
+						picture.setLayoutParams(new FrameLayout.LayoutParams(
+								LayoutParams.FILL_PARENT, (int) (tile
+										.getMeasuredHeight() * 0.33f)));
 						picture.setImageBitmap(bitmap);
-						title.setBackgroundColor(Color.argb(0xbb, 0x37, 0x39, 0x37));
+						title.setBackgroundColor(Color.argb(0xbb, 0x37, 0x39,
+								0x37));
 					}
 				}).execute(story.getString("imageUrl"));
 			}
 			tile_group[i / 3].addView(tile);
 		}
+	}
+
+	private String formatDate(JSONObject story) {
+		try {
+			Date date = org.apache.http.impl.cookie.DateUtils
+					.parseDate(
+							story.getString("publishDate"),
+							new String[] { org.apache.http.impl.cookie.DateUtils.PATTERN_RFC1123 });
+			return DateUtils.getRelativeDateTimeString(getActivity(),
+					date.getTime(), DateUtils.MINUTE_IN_MILLIS,
+					DateUtils.WEEK_IN_MILLIS, 0).toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (DateParseException e) {
+			e.printStackTrace();
+		}
+		return "";
 	}
 
 }
